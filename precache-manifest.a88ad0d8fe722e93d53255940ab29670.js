8ba80280c95d312f2dd375a54a4a62bb48ea5594
queite <queite@users.noreply.github.com>
2022-04-20 18:51:45 +0000

    deploy: cfb4cc0c1a02957aa77ff08a66a6c2535081a573

---
+++ precache-manifest.a88ad0d8fe722e93d53255940ab29670.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0214325a789f95b7c5bcd13b5e0cc6fb",
+    "url": "/star-wars-planets/index.html"
+  },
+  {
+    "revision": "39479e02ff8bef721329",
+    "url": "/star-wars-planets/static/css/main.0a97e9ea.chunk.css"
+  },
+  {
+    "revision": "54aed8bc1e4e6356f210",
+    "url": "/star-wars-planets/static/js/2.77584d87.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/star-wars-planets/static/js/2.77584d87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39479e02ff8bef721329",
+    "url": "/star-wars-planets/static/js/main.b437ff66.chunk.js"
+  },
+  {
+    "revision": "6b480a4a8b5688e80087",
+    "url": "/star-wars-planets/static/js/runtime-main.6f08aa3c.js"
+  },
+  {
+    "revision": "952e2ba8e55e299663b195b3ca72d83e",
+    "url": "/star-wars-planets/static/media/icons8-guerra-nas-estrelas.952e2ba8.svg"
+  }
+]);
